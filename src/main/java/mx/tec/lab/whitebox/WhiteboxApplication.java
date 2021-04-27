df6c58ae407ed48384f8e423641d81cecf338b91
gracklewontsing <jera_3676@hotmail.com>
2021-04-27 16:43:36 -0600

    first commit

---
+++ src/main/java/mx/tec/lab/whitebox/WhiteboxApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.lab.whitebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/tec/lab/whitebox/WhiteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.tec.lab.whitebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
