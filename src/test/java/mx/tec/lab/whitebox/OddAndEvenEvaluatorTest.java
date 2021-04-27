d0403956aaff2ab3c29f6330bc70666882b3d8fc
gracklewontsing <jera_3676@hotmail.com>
2021-04-27 17:53:42 -0600

    added test cases for full percentage

--- src/test/java/mx/tec/lab/whitebox/OddAndEvenEvaluatorTest.java
+++ src/test/java/mx/tec/lab/whitebox/OddAndEvenEvaluatorTest.java
@@ -15,6 +15,28 @@ public class OddAndEvenEvaluatorTest {
 		// When
 		String result = evaluator.evaluate(a, b);
 		// Then
-		assertEquals("C is even and D is even, c=4.0 and d=0.0", result);
+		assertEquals("c is even, d even, c=4.0 and d=0.0", result);
+  }
+
+  @Test
+  public void givenA3B2WhenEvaluateThenCOddDOdd(){
+    // Given
+		double a = 3;
+		double b = 2;
+		// When
+		String result = evaluator.evaluate(a, b);
+		// Then
+		assertEquals("c is odd and d is odd, c=5.0 and d=1.0", result);
+  }
+
+  @Test
+  public void givenA8B5WhenEvaluateThenCOddDOdd(){
+    // Given
+		double a = 8;
+		double b = 5;
+		// When
+		String result = evaluator.evaluate(a, b);
+		// Then
+		assertEquals("c is odd and d is odd, c=13.0 and d=3.0", result);
   }
 }
